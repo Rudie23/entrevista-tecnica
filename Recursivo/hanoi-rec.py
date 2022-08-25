f411b878a2ce7405a79fbbddc309df7be0309329
Diego <ruandiego100@gmail.com>
2022-08-25 15:17:48 -0300

    Revisando Hanoi Recursivo

--- Recursivo/hanoi-rec.py
+++ Recursivo/hanoi-rec.py
@@ -18,7 +18,6 @@ def hanoi(n, orig='A', aux='B', dest='C'):
         hanoi(n - 1, aux, orig, dest)  # Segunda Chamada
 
 
-hanoi(2)
-for i in range(1, 4):
+for i in range(3, 4):
     print(f'### Hanoi {i}')
     hanoi(i)
